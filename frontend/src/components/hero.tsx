a0ae3e68c9c02a48261e1e34aec1a036c630a82c
“farah-jannat” <farahjannatmithila@gmail.com>
2026-02-25 18:29:47 +0600

    feat: add hero component

---
+++ frontend/src/components/hero.tsx
@@ -0,0 +1,12 @@
+import Carousel from "@/components/carousel";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="py-[30px]">
+      <Carousel />
+    </div>
+  );
+};
+
+export default Hero;
