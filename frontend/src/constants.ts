686125dca77ab04963c9e2ce1f48496937d5f3d7
“farah-jannat” <farahjannatmithila@gmail.com>
2026-02-26 20:51:02 +0600

    chore: required data

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -140,78 +140,11 @@ export const products = [
   },
 ];
 
-export const products2 = [
-  {
-    id: 1,
-    title: "Pregnancy HCG Test Midstream Strip",
-    image:
-      "https://www.arogga.com/_next/image?url=https%3A%2F%2Fcdn2.arogga.com%2FeyJidWNrZXQiOiJhcm9nZ2EiLCJrZXkiOiJQcm9kdWN0LXBfaW1hZ2VzXC82MzIxNFwvNjMyMTQtV2hhdHNBcHAtSW1hZ2UtMjAyNS0wMy0wMy1hdC0zLWw4YzB0ai5qcGVnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjoyMDAsImhlaWdodCI6MjAwLCJmaXQiOiJvdXRzaWRlIn19fQ%3D%3D&w=256&q=75",
-    oldPrice: 120,
-    price: 20,
-    discount: 83,
-    rating: 5,
-    reviews: 33,
-    delivery: "12-24 HOURS",
-  },
-  {
-    id: 2,
-    title: "Napa Extend 665mg Tablet (10s)",
-    image:
-      "https://www.arogga.com/_next/image?url=https%3A%2F%2Fcdn2.arogga.com%2FeyJidWNrZXQiOiJhcm9nZ2EiLCJrZXkiOiJQcm9kdWN0LXBfaW1hZ2VzXC85MDc3MFwvOTA3NzAtRm9zdGVyLUNsYXJrLXMtTWFuZ28tRmxhdm91cmVkLUluc3RhbnQtRHJpbmstUG93ZGVyLTUwMGctZzloM3g5LndlYnAiLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjIwMCwiaGVpZ2h0IjoyMDAsImZpdCI6Im91dHNpZGUifX19&w=256&q=75",
-    oldPrice: 150,
-    price: 135,
-    discount: 10,
-    rating: 4,
-    reviews: 120,
-    delivery: "INSTANT",
-  },
-  {
-    id: 3,
-    title: "Savlon Antiseptic Liquid 500ml",
-    image:
-      "https://www.arogga.com/_next/image?url=https%3A%2F%2Fcdn2.arogga.com%2FeyJidWNrZXQiOiJhcm9nZ2EiLCJrZXkiOiJQcm9kdWN0LXBfaW1hZ2VzXC85MDEyNlwvOTAxMjYtOTAxMjYteXQxZ2g1LnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MjAwLCJoZWlnaHQiOjIwMCwiZml0Ijoib3V0c2lkZSJ9fX0%3D&w=256&q=75",
-    oldPrice: 280,
-    price: 265,
-    discount: 5,
-    rating: 5,
-    reviews: 89,
-    delivery: "12-24 HOURS",
-  },
-  {
-    id: 4,
-    title: "Dettol Original Soap 125g",
-    image:
-      "https://www.arogga.com/_next/image?url=https%3A%2F%2Fcdn2.arogga.com%2FeyJidWNrZXQiOiJhcm9nZ2EiLCJrZXkiOiJQcm9kdWN0LXBfaW1hZ2VzXC85MDEyNVwvOTAxMjUtOTAxMjUtNTA1YjdoLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MjAwLCJoZWlnaHQiOjIwMCwiZml0Ijoib3V0c2lkZSJ9fX0%3D&w=256&q=75",
-    oldPrice: 75,
-    price: 70,
-    discount: 7,
-    rating: 4,
-    reviews: 245,
-    delivery: "24-48 HOURS",
-  },
-  {
-    id: 5,
-    title: "Sensodyne Fresh Mint Toothpaste",
-    image:
-      "https://www.arogga.com/_next/image?url=https%3A%2F%2Fcdn2.arogga.com%2FeyJidWNrZXQiOiJhcm9nZ2EiLCJrZXkiOiJQcm9kdWN0LXBfaW1hZ2VzXC85MDc3M1wvOTA3NzMtRm9zdGVyLUNsYXJrLXMtTWFuZ28tRmxhdm91cmVkLUluc3RhbnQtRHJpbmstUG93ZGVyLTJLZy1KYXItOTA1d2ZwLndlYnAiLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjIwMCwiaGVpZ2h0IjoyMDAsImZpdCI6Im91dHNpZGUifX19&w=256&q=75",
-    oldPrice: 210,
-    price: 180,
-    discount: 14,
-    rating: 5,
-    reviews: 56,
-    delivery: "12-24 HOURS",
-  },
-
-  {
-    id: 5,
-    title: "Sensodyne Fresh Mint Toothpaste",
-    image:
-      "https://www.arogga.com/_next/image?url=https%3A%2F%2Fcdn2.arogga.com%2FeyJidWNrZXQiOiJhcm9nZ2EiLCJrZXkiOiJQcm9kdWN0LXBfaW1hZ2VzXC84NjEzNlwvODYxMzYtc2tpbi1PLWh5ZHJhdGluZy1sb3Rpb24tMi0xLXJndnUyOS5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjIwMCwiaGVpZ2h0IjoyMDAsImZpdCI6Im91dHNpZGUifX19&w=256&q=75",
-    oldPrice: 210,
-    price: 180,
-    discount: 14,
-    rating: 5,
-    reviews: 56,
-    delivery: "12-24 HOURS",
-  },
+export const bannerImages = [
+  "https://www.arogga.com/_next/image?url=https%3A%2F%2Fcdn2.arogga.com%2FeyJidWNrZXQiOiJhcm9nZ2EiLCJrZXkiOiJCbG9jay1iX2NvbmZpZ1wvMFwvMTEzLVdlYi1iYW5uZXItOTgwLXgtNDI4LWtqeWx6YS5qcGVnIiwiZWRpdHMiOltdfQ%3D%3D&w=1280&q=75",
+  "https://www.arogga.com/_next/image?url=https%3A%2F%2Fcdn2.arogga.com%2FeyJidWNrZXQiOiJhcm9nZ2EiLCJrZXkiOiJCbG9jay1iX2NvbmZpZ1wvMFwvMTEzLVJhbWFkYW4tV0VCQkFOTkVSLXR1ZHhudi5wbmciLCJlZGl0cyI6W119&w=1280&q=75",
+  "https://www.arogga.com/_next/image?url=https%3A%2F%2Fcdn2.arogga.com%2FeyJidWNrZXQiOiJhcm9nZ2EiLCJrZXkiOiJCbG9jay1iX2NvbmZpZ1wvMFwvMTEzLWFyb2dnYS1vNTdycHQucG5nIiwiZWRpdHMiOltdfQ%3D%3D&w=1280&q=75",
+  "https://www.arogga.com/_next/image?url=https%3A%2F%2Fcdn2.arogga.com%2FeyJidWNrZXQiOiJhcm9nZ2EiLCJrZXkiOiJCbG9jay1iX2NvbmZpZ1wvMFwvMTEzLVBOUy1QYW5kYW1hcnQtQmFubmVyXzI1NjAteC0xMDk3LXB4LWZ3cHB6aS5wbmciLCJlZGl0cyI6W119&w=1280&q=75",
+  "/hero4.avif",
+  "https://www.arogga.com/_next/image?url=https%3A%2F%2Fcdn2.arogga.com%2FeyJidWNrZXQiOiJhcm9nZ2EiLCJrZXkiOiJCbG9jay1iX2NvbmZpZ1wvMFwvMTEzLWFyb2dnYS1sYWItc2o0YjNyLnBuZyIsImVkaXRzIjpbXX0%3D&w=1280&q=75",
 ];
