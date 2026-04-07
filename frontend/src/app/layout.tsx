136ab10424d3c47aa7e07d279aa908e7b9065192
“farah-jannat” <farahjannatmithila@gmail.com>
2026-04-07 17:01:06 +0600

    feat: add toaster in layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "sonner";
+import Provider from "@/components/provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <Toaster position="bottom-center" richColors />
+        <Provider>{children}</Provider>
+        {/* {children} */}
       </body>
     </html>
   );
