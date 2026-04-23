6fc80e8b7a0a8a49c1958faed98e713cf81d2c89
“farah-jannat” <farahjannatmithila@gmail.com>
2026-04-23 14:44:40 +0600

    fix: update product-options-schema

--- frontend/src/features/dashboards/schemas/dashboard-product-form.schema.ts
+++ frontend/src/features/dashboards/schemas/dashboard-product-form.schema.ts
@@ -1,8 +1,9 @@
 import { Weight } from "lucide-react";
 import { z } from "zod";
 
-const imgSchema = z.object({
-  url: z.string().url({ message: "Invalid URL format for image." }),
+export const imgSchema = z.object({
+  // id:z.string(),
+  url: z.string()
 });
 
 // imgs: z.array(z.string());
@@ -15,7 +16,25 @@ const additionalInfoSchema = z.object({
 const productOptionSchema = z.object({
   name: z.string(),
   type: z.string(),
-  choices: z.string(),
+  choices: z
+    .array(
+      z.object({
+        value: z
+          .string()
+          .trim()
+          .min(1, "Choice cannot be empty")
+          .max(50, "Keep it under 50 characters"),
+      }),
+    )
+    .min(1, "Please add at least one choice")
+    // Custom refinement to check for duplicates across the object values
+    .refine(
+      (items) => {
+        const values = items.map((item) => item.value.toLowerCase());
+        return new Set(values).size === values.length;
+      },
+      { message: "Each choice must be unique" },
+    ),
 });
 
 const productVarientsSchema = z.object({
@@ -40,3 +59,5 @@ export const dashProductSchema = z.object({
 });
 
 export type DashProductType = z.infer<typeof dashProductSchema>;
+export type ProductImage = z.infer<typeof imgSchema>;
+export type ProductAdditionalInfo = z.infer<typeof additionalInfoSchema>;
